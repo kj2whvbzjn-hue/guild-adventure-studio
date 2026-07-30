777c7d7d79946458afbe34da07012180f0ecea3a
kj2whvbzjn-hue <kj2whvbzjn@privaterelay.appleid.com>
2026-07-30 12:16:09 +0900

    Deploy Guild Adventure Studio update 1.23.0-dev

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME='guild-adventure-v003-build392';
-const OFFLINE_URL='./index.html?appv=392';
+const CACHE_NAME='guild-adventure-v003-build393';
+const OFFLINE_URL='./index.html?appv=393';
 const APP_SHELL=[
   './',
-  './index.html?appv=392',
+  './index.html?appv=393',
   './manifest.webmanifest?v=380',
   './icon-192.png?v=380',
   './icon-512.png?v=380'
