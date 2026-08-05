d2c2f79e37ba2ee9d80966aa8d51b379dee89676
kj2whvbzjn-hue <kj2whvbzjn@privaterelay.appleid.com>
2026-08-06 08:38:36 +0900

    Deploy Guild Adventure Studio update GA-B481 / GKS-B484

--- tools/test_gks_g003_studio_definition.js
+++
@@ -1,8 +0,0 @@
-const fs=require('fs'),path=require('path');
-const html=fs.readFileSync(path.join(__dirname,'../studio/index.html'),'utf8');
-function check(v,m){if(!v)throw new Error(m)}
-for(const x of ['GKS-G003_TAG_DRIVEN_COMBAT_EFFECTS_BEGIN','hp_ratio_at_most','hp_ratio_at_least','spend_hp','deal_damage','heal','target_max_hp'])check(html.includes(x),'missing '+x);
-const schema=JSON.parse(fs.readFileSync(path.join(__dirname,'../schemas/skill.schema.json'),'utf8'));
-const types=schema.$defs.operation.properties.type.enum;
-for(const x of ['spend_hp','deal_damage','heal'])check(types.includes(x),'schema missing '+x);
-console.log('GKS-G003 STUDIO PASS');
