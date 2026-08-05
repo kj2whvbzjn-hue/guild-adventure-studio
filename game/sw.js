de0eb7843879e39522fd5f1438cb033348707d35
kj2whvbzjn-hue <kj2whvbzjn@privaterelay.appleid.com>
2026-08-05 19:19:32 +0900

    Deploy Guild Adventure Studio update 1.23.0-dev

--- game/sw.js
+++ game/sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME='guild-adventure-ga-b464';
-const OFFLINE_URL='./index.html?appv=464';
+const CACHE_NAME='guild-adventure-ga-b465';
+const OFFLINE_URL='./index.html?appv=465';
 const APP_SHELL=[
   './',
-  './index.html?appv=464',
-  './manifest.webmanifest?v=464',
-  './icon-192.png?v=464',
-  './icon-512.png?v=464'
+  './index.html?appv=465',
+  './manifest.webmanifest?v=465',
+  './icon-192.png?v=465',
+  './icon-512.png?v=465'
 ];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('guild-adventure-')&&key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
