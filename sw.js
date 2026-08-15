aa82df76e90e7f9ff18788da90a0f37a97704443
kj2whvbzjn-hue <kj2whvbzjn@privaterelay.appleid.com>
2026-08-15 17:15:51 +0900

    Deploy Guild Adventure Studio update GKS-B577

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME="ga-root-b486180-b576";
+const CACHE_NAME="ga-root-b486180-b577";
 const CACHE_PREFIX="ga-root-";
-const OFFLINE_URL='./index.html?appv=486180b576';
+const OFFLINE_URL='./index.html?appv=486180b577';
 const APP_SHELL=[
   "./",
-  "./index.html?appv=486180b576",
+  "./index.html?appv=486180b577",
   "./manifest.webmanifest?v=4861",
   "./icon-192.png?v=4861",
   "./icon-512.png?v=4861"
